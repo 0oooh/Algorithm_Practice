bbedd78f9cb886c0c1487babf33ea689a9e8b868
0oooh <155450025+0oooh@users.noreply.github.com>
2025-01-15 20:46:49 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 3208 ms, Memory: 55612 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,26 @@
+from functools import cmp_to_key
+
+N = int(input())
+new_list = []
+for age_name in range(N):
+    x, y = map(int, input().split())
+    new_list.append([y, x])
+
+
+def compare(x, y):
+    if (x[0] > y[0]):  # x[0] 값이 y[0]값 보다 작으면
+        return 1  # y 내용을 앞으로 보냄
+    elif (x[0] < y[0]):
+        return -1
+    else:  # x[0] 값이 y[0]값과 동일하면
+        if (x[1] < y[1]):  # x[1]과 y[1]을 비교해서 y[1]이 크면
+            return -1  # x 내용을 앞으로 보냄
+        elif (x[1] > y[1]):
+            return 1
+        else:
+            return 0
+
+
+final = sorted(new_list, key=cmp_to_key(compare))
+for num in final:
+    print(num[1], num[0])
