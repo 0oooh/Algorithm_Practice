dd1c189a849c318627e2238c2b1481950a665c06
Seohyeon Chang <155450025+0oooh@users.noreply.github.com>
2025-01-29 21:24:26 +0900

    [Silver II] Title: 스택 수열, Time: 1052 ms, Memory: 41984 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,28 +1,28 @@
-import sys
 from collections import deque
+import sys
+n = int(sys.stdin.readline())  # 목표 수열의 길이
+y = deque(int(sys.stdin.readline()) for _ in range(n))  # 목표 수열 저장
 
-n = int(input())
-y = deque([int(input()) for _ in range(n)])  # 목표 수열
+x = [i for i in range(1, n + 1)]  # 1부터 n까지 넣을 수 있는 리스트
+memory = []  # 스택 역할
+result = []  # 연산 결과 저장
 
+for i in range(len(y)):
+    target = y[i]
 
-x = []  # 스택 역할 (push)
-memory = []  # 실제 pop할 때 사용하는 스택 역할
+    while not memory or memory[-1] != target:  # memory가 비었거나, 원하는 값이 없으면 push
+        if x:  # x에 값이 있으면 memory에 push
+            memory.append(x.pop(0))
+            result.append("+")
+        else:  # x가 비었는데도 target을 못 찾으면 NO 출력
+            print("NO")
+            exit()
 
-result = []  # 연산 저장
-
-num = 1  # push할 숫자
-
-for target in y:
-    while num <= target:  # 목표 숫자까지 push 진행
-        x.append(num)
-        result.append("+")
-        num += 1  # 다음 숫자 준비
-
-    if x and x[-1] == target:  # 스택의 top이 목표 숫자면 pop
-        x.pop()
+    if memory and memory[-1] == target:  # memory의 top이 target이면 pop
+        memory.pop()
         result.append("-")
     else:
-        print("NO")  # 만들 수 없는 수열
+        print("NO")
         exit()
 
-print("\n".join(result))  # 결과 출력
+print("\n".join(result))  # 연산 결과 출력
