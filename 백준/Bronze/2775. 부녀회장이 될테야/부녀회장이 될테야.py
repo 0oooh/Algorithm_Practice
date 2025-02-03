37bca514e2a8ad5c0e73cd9f498936f8fe9834f1
Seohyeon Chang <155450025+0oooh@users.noreply.github.com>
2025-02-03 16:08:48 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,22 +1,19 @@
+final = []
 T = int(input())  # 테스트 케이스 개수 입력
 
-final = []
 for _ in range(T):
     k = int(input())  # 층 입력
     n = int(input())  # 호 입력
 
-    def fibo(memo, j, k, n):
-        for floor in range(k):  # k층까지 반복
-            for i in range(n):  # 1호부터 n호까지 누적 덧셈
-                if i == 0:
-                    memo[i] = memo[i]  # 첫 번째 요소 유지
-                else:
-                    memo[i] = memo[i] + memo[i - 1]  # 누적 덧셈 적용
-
-    memo = [i for i in range(1, n + 1)]  # 0층 초기화: i호에는 i명 거주
+    memo = [i for i in range(1, n + 1)]  # 0층 초기화: i호에는 i명이 거주
     j = 1  # j는 따로 사용되지 않지만 유지
 
-    fibo(memo, j, k, n)  # 계산 수행
-    final.append(memo[-1])
+    for floor in range(k):  # k층까지 반복
+        for i in range(n):  # 1호부터 n호까지 누적 덧셈
+            if i == 0:
+                memo[i] = memo[i]  # 첫 번째 호는 그대로 유지
+            else:
+                memo[i] = memo[i] + memo[i - 1]  # 이전 값 더하기
 
-print(*final, sep="\n")
+    final.append(memo[n - 1])  # 최종 결과 출력
+print(*final, sep='\n')
