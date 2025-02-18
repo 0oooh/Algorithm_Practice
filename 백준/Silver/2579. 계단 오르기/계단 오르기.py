c9ffbd82a616b27ff888c1a0f27ea24e04823133
Seohyeon Chang <155450025+0oooh@users.noreply.github.com>
2025-02-18 19:04:45 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+Stair = [0]
+dp = [0]*(N+1)
+
+for i in range(1,N+1):
+    Stair.append(int(input()))
+    if i == 1 :
+        dp[1] = Stair[1]
+    elif i == 2 :
+        dp[2] = Stair[1] + Stair[2]
+        
+    else :
+        dp[i] = max(Stair[i]+Stair[i-1]+dp[i-3], Stair[i]+dp[i-2])
+
+print(dp[N])
