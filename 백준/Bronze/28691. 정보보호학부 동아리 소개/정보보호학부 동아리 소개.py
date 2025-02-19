5e5b4419f1714e91f635f234edab1aa56dbc7160
Seohyeon Chang <155450025+0oooh@users.noreply.github.com>
2025-02-19 13:05:57 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,16 @@
+word = input()
+if word.startswith("M"):
+    print("MatKor")
+
+elif word.startswith("W"):
+    print("WiCys")
+
+elif word.startswith("C"):
+    print("CyKor")
+
+elif word.startswith("A"):
+    print("AlKor")
+
+
+elif word.startswith("$"):
+    print("$clear")
